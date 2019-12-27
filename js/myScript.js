0237c29bdb8ab531740747db29c5439bb2158629
lega z50 <legavaz@gmail.com>
2019-12-27 03:06:18 +0300

    2

---
+++ js/myScript.js
@@ -0,0 +1,13 @@
+
+function returnFileName() {
+            mElement     =   document.getElementById("id_fileName");
+ 
+            if (mElement.innerHTML=="") {
+             mElement.innerHTML  =  "name file: "+ document.baseURI ;
+             
+            } else {
+             
+             mElement.innerHTML  =  "" ;    
+             
+            }
+}
---
+++ js/twoPeremen.js
@@ -0,0 +1,13 @@
+
+
+var movie = "Крестный отец"
+console.log(movie);
+
+function showGoodMovie() {
+    alert(movie+" - хороший фильм")    
+}
+
+function showBadMovie() {
+    var movie   =   "Знакомьтесь Джо блек"
+    alert(movie+" - плохой фильм")    
+}
